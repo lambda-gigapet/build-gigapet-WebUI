4afd2d205746f7c6fae285ca9b26f16bfe35c66c
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-04-18 11:04:29 -0700

    think all js works final testing before moving on

--- js/about.js
+++ js/about.js
@@ -3,17 +3,18 @@ class Programmer {
       // assign this.domElement to the passed in domElement
       this.domElement = domElement;
       // create a reference to the ".expandButton" class. 
-      this.expandButton = this.domElement.querySelector('.programmer');
+        this.expandButton = this.domElement.querySelector('.programmer');
       // Set a click handler on the expandButton reference, calling the expandArticle method.
       this.expandButton.addEventListener('click', () => this.expandArticle());
     }
-  
+
+
     expandArticle() {
       // Using our reference to the domElement, toggle a class to expand or hide the article.
       this.domElement.classList.toggle('programmer-open');
     }
   }
-  
+
   /* START HERE: 
   
   - Select all classes named ".article" and assign that value to the articles variable.  
